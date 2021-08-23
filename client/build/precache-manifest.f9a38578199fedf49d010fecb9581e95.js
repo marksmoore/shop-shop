01451be3a99d2f4fe783b4aa1893c877f9154ede
Marco <msmo1960@gmail.com>
2021-08-23 17:09:41 -0500

    updating package.json

---
+++ client/build/precache-manifest.f9a38578199fedf49d010fecb9581e95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e9e94bc7e325ca6ea1e4ea6bc9b9d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc5c96786bd09dfda08f",
+    "url": "/static/css/main.eb8551ff.chunk.css"
+  },
+  {
+    "revision": "ac2b2cfa0250885ee904",
+    "url": "/static/js/2.a3505887.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.a3505887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5c96786bd09dfda08f",
+    "url": "/static/js/main.ddfbc843.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
